0b952e2a3195ba8799b64174f1802d156074d2be
henriquedeof <henriquedeof@gmail.com>
2020-03-29 11:47:15 +1100

    Adding relationships.

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,41 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne //I did not add cascade here because I do not want to delete a RECIPE if a NOTES object is deleted.
+    private Recipe recipe;
+
+    @Lob //Annotation for large objects. By default, the quantity of characters for JPA and Hibernate is 255. In this case, there is the possibility to have more than that.
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
