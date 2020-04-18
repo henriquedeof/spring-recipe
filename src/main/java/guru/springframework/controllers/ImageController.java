22aa9f7fa8fb34399f376d8743cd836c98528209
henriquedeof <henriquedeof@gmail.com>
2020-04-18 21:37:54 +1000

    Image renderer added.

--- src/main/java/guru/springframework/controllers/ImageController.java
+++ src/main/java/guru/springframework/controllers/ImageController.java
@@ -1,8 +1,10 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.ImageService;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @Controller
@@ -40,5 +47,24 @@ public class ImageController {
         return "redirect:/recipe/" + idRecipe + "/show";
     }
 
+    @GetMapping("/recipe/{idRecipe}/recipeimage")
+    public void renderImageFromDB(@PathVariable String idRecipe, HttpServletResponse response) throws IOException {
+        log.debug("Rendering image from DB");
+
+        RecipeCommand recipeCommand = this.recipeService.findCommandById(Long.valueOf(idRecipe));
+
+        if (recipeCommand.getImage() != null) {
+            byte[] bytes = new byte[recipeCommand.getImage().length];
+
+            int i = 0;
+            for (Byte wrappedByte : recipeCommand.getImage()) {
+                bytes[i++] = wrappedByte; //auto unboxing
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream inputStream = new ByteArrayInputStream(bytes);
+            IOUtils.copy(inputStream, response.getOutputStream());
+        }
+    }
 
 }
