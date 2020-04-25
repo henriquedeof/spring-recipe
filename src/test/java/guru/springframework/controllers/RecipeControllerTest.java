2c3bb7a12ddd0727a3e4cf95771c9688adc48056
henriquedeof <henriquedeof@gmail.com>
2020-04-25 14:20:20 +1000

    Spring MVC exception handling.

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -104,6 +104,8 @@ class RecipeControllerTest {
         when(this.service.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
+
 }
